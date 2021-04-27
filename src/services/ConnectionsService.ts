f246963d80fd66c6aee5d0d3e453fb9882776231
Luiz Oliveira <luiiz.silverio@gmail.com>
2021-04-27 13:08:00 -0300

    Aula 5

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -9,7 +9,7 @@ interface IConnectionCreate {
    id?: string;
 }
 
-class ConnectionsService {
+class ConnectionsServices {
    private connectionsRepository: Repository<Connection>
 
    constructor() {
@@ -39,6 +39,33 @@ class ConnectionsService {
 
       return connection
    }
+
+   async findAllNoAdmin() {
+      const connections = await this.connectionsRepository.find({
+         where: { admin_id: null },
+         relations: ["user"]
+      })
+
+      return connections
+   }
+
+   async findBySocketID(socket_id: string) {
+      const connection = await this.connectionsRepository
+         .findOne({ socket_id })
+
+      return connection
+   }
+
+   async updateAdminID(user_id: string, admin_id: string) {
+      await this.connectionsRepository
+         .createQueryBuilder()
+         .update(Connection)
+         .set({ admin_id })
+         .where('user_id = :user_id', {
+            user_id
+         })
+         .execute()
+   }
 }
 
-export { ConnectionsService }
+export { ConnectionsServices }
